823f99908aebef24cc78dc2a4d639e41db9ea3ba
juki3r <deepong25@gmail.com>
2024-03-08 18:43:39 +0800

    modified:   resources/views/layouts/navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -101,6 +101,9 @@
                 <x-responsive-nav-link :href="route('newapplication')" :active="request()->routeIs('newapplication')">
                     {{ __('NewApplication') }}
                 </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('users')" :active="request()->routeIs('users')">
+                    {{ __('Users') }}
+                </x-responsive-nav-link>
             @else
                 <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                     {{ __('Dashboard') }}
