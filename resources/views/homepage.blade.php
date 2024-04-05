4cfaab94097ea3b82195bda61ce24c50dbb33213
juki3r <deepong25@gmail.com>
2024-04-06 00:10:26 +0800

    modified:   resources/views/homepage.blade.php

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -103,7 +103,7 @@
                       </span>
                       <h3 class="fs-2">Guaranted Speed</h3>
                       <p class="fs-5">
-                        A trusted internet speed for your needs with back-up switching for non-stop activities
+                        A trusted internet speed for your needs with back-up switching for non-stop activities online.
                       </p>
                     </div>
                   </div>
@@ -112,30 +112,30 @@
                   <div class="col-12 col-md-6" style="background-color:rgb(10, 48, 3)">
                     <div class="card p-4 text-light">
                       <span class="mt-5">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-clock text-light border p-2" viewBox="0 0 16 16">
-                          <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
-                          <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-battery-charging text-light border p-2" viewBox="0 0 16 16">
+                          <path d="M9.585 2.568a.5.5 0 0 1 .226.58L8.677 6.832h1.99a.5.5 0 0 1 .364.843l-5.334 5.667a.5.5 0 0 1-.842-.49L5.99 9.167H4a.5.5 0 0 1-.364-.843l5.333-5.667a.5.5 0 0 1 .616-.09z"/>
+                          <path d="M2 4h4.332l-.94 1H2a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h2.38l-.308 1H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2"/>
+                          <path d="M2 6h2.45L2.908 7.639A1.5 1.5 0 0 0 3.313 10H2zm8.595-2-.308 1H12a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H9.276l-.942 1H12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2z"/>
+                          <path d="M12 10h-1.783l1.542-1.639q.146-.156.241-.34zm0-3.354V6h-.646a1.5 1.5 0 0 1 .646.646M16 8a1.5 1.5 0 0 1-1.5 1.5v-3A1.5 1.5 0 0 1 16 8"/>
                         </svg>
                       </span>
-                      <h3 class="fs-2">100% Uptime Transmit</h3>
+                      <h3 class="fs-2">With Back-up Power</h3>
                       <p class="fs-5">
-                        Multiple ISP's that switches automatically for non-stop 
-                        Transmit and Recieve sessions.
+                        Guaranted back-up power to avoid internet disruption cause by electricity failure.
                       </p>
                     </div>
                   </div>
                   <div class="col-12 col-md-6" style="background-color:rgb(88, 27, 3)">
                     <div class="card p-4 text-light">
                       <span class="mt-5">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-clock text-light border p-2" viewBox="0 0 16 16">
-                          <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
-                          <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-tools text-light border p-2" viewBox="0 0 16 16">
+                          <path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.27 3.27a.997.997 0 0 0 1.414 0l1.586-1.586a.997.997 0 0 0 0-1.414l-3.27-3.27a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3q0-.405-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814zm9.646 10.646a.5.5 0 0 1 .708 0l2.914 2.915a.5.5 0 0 1-.707.707l-2.915-2.914a.5.5 0 0 1 0-.708M3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026z"/>
                         </svg>
                       </span>
-                      <h3 class="fs-2">100% Uptime Transmit</h3>
+                      <h3 class="fs-2">Other Services Offer</h3>
                       <p class="fs-5">
-                        Multiple ISP's that switches automatically for non-stop 
-                        Transmit and Recieve sessions.
+                        Computer & Laptop repair, Online Services and Assistance, Website making,
+                        Pisowifi repair, and Home Automation(Electrical and Mechanical).
                       </p>
                     </div>
                   </div>
