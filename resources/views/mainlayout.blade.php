5c053fabe14034f54614628a11a0f1242f54d3f5
juki3r <deepong25@gmail.com>
2024-04-05 20:45:12 +0800

    modified:   public/css/main.css 	modified:   resources/views/mainlayout.blade.php

--- resources/views/mainlayout.blade.php
+++ resources/views/mainlayout.blade.php
@@ -15,16 +15,16 @@
     {{-- NAVBAR --}}
     <nav class="navbar navbar-expand-lg bg-body-tertiary p-0 m-0">
         <div class="container-fluid navBar">
-            <a class="navbar-brand" href="/">
-                <img src="{{asset('assets/ajclogo.png')}}" alt="LOGO" width="120px">
+            <a class="navbar-brand ms-5" href="/">
+                <img src="{{asset('assets/ajclogo.png')}}" alt="LOGO"  class="logo">
             </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon">
-                
-            </span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-list text-danger" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                  </svg>
           </button>
           <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav me-auto mb-3 mb-lg-0" style="width: 100%">
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
               </li>
@@ -32,10 +32,7 @@
                 <a class="nav-link" href="#">Link</a>
               </li>
             </ul>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+          
           </div>
         </div>
       </nav>
