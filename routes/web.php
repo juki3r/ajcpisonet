68a634ef9a247f3b9d661ce8fb8372b0132a7342
juki3r <deepong25@gmail.com>
2024-04-05 18:08:58 +0800

    deleted:    resources/views/admindashboard.blade.php 	deleted:    resources/views/dashboard.blade.php 	deleted:    resources/views/homepage.blade.php 	deleted:    resources/views/include/100mbps.blade.php 	deleted:    resources/views/include/20mbps.blade.php 	deleted:    resources/views/include/30mbps.blade.php 	deleted:    resources/views/include/50mbps.blade.php 	deleted:    resources/views/include/applynow.blade.php 	deleted:    resources/views/include/bill.blade.php 	deleted:    resources/views/include/billedit.blade.php 	deleted:    resources/views/include/contactus.blade.php 	deleted:    resources/views/include/internet.blade.php 	deleted:    resources/views/include/modifyclient.blade.php 	deleted:    resources/views/include/modifyuser.blade.php 	deleted:    resources/views/include/newapplication.blade.php 	deleted:    resources/views/include/promos.blade.php 	deleted:    resources/views/include/users.blade.php 	modified:   resources/views/layouts/navigation.blade.php 	deleted:    resources/views/linkaccount.blade.php 	deleted:    resources/views/mainlayout.blade.php 	deleted:    resources/views/navbar.blade.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -27,43 +27,7 @@ Route::get('/', [AJCController::class, 'homepage']);
 
 Route::middleware(['auth', 'verified'])->group(function () {
 
-    Route::get('/dashboard/', [DashboardController::class, 'dashboard'])->name('dashboard');
-    Route::get('/admindashboard', [AllclientController::class, 'admindashboard'])->name('admindashboard');
-    Route::get('/bill', [AJCController::class, 'bill'])->name('bill');
-
-    // Link account
-    Route::get('/linkaccount', [ApplicationController::class, 'links'])->name('linkaccount');
-    Route::post('/linkaccount', [ApplicationController::class, 'linkaccount'])->name('linkaccount');
-
-    // CRUD clients
-    Route::get('createclient/{id}', [AllclientController::class, 'modi'])->name('edit');
-
-    // Route::get('clients/{id}', [AllclientController::class, 'edit'])->name('edit');
-    Route::put('clients/{id}', [AllclientController::class, 'update']);
-    Route::get('clients/{id}/delete', [AllclientController::class, 'deleteclient']);
-
-    // For new Application admin dashboard
-    Route::get('/newapplication', [AllclientController::class, 'newapplication'])->name('newapplication');
-    Route::get('newapplication/{id}/delete', [AllclientController::class, 'deletenewapplication']);
-
-    //Users View Blade
-    Route::get('/users', [AllclientController::class, 'newuser'])->name('users');
-    Route::get('usertype/{id}', [AllclientController::class, 'modiuser'])->name('modi');
-    Route::put('usertype/{id}', [AllclientController::class, 'modiUsers']);
-
-    // For linking user dashboard
-
-    Route::post('/link',[DashboardController::class, 'linkaccount'])->name('linkaccount');
-
-    // For Approve client auto add and auto delete from new application
-    Route::get('/approveclient/{id}/{firstname}/{lastname}/{email}/{contact}/{birthday}/{gender}/{plan}/{street}/{barangay}/{town}/{province}', [ApplicationController::class, 'approveclient'])->name('approveclient');
-
-
-    //For Bill admin side
-    Route::get('clients/{id}/bill', [AllclientController::class, 'bill']);
-    Route::get('clients/{client_id}/billedit', [AllclientController::class, 'billedit']);
-    Route::put('clients/{client_id}/{fullname}/{accountnumber}/{plan}', [AllclientController::class, 'billupdate'])->name('billupdate');
-
+   
     // Mailing
     Route::get('clients/{email}/{fullname}/{accountnumber}', function($email, $fullname, $accountnumber){
         //Send email to client Bill Reminder
@@ -81,31 +45,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/apply_899',function(){
-    return view('include.20mbps');
-})->name('20mbps');
-Route::get('/apply_1000',function(){
-    return view('include.30mbps');
-})->name('30mbps');
-Route::get('/apply_1300',function(){
-    return view('include.50mbps');
-})->name('50mbps');
-Route::get('/apply_1500',function(){
-    return view('include.100mbps');
-})->name('100mbps');
 
 require __DIR__.'/auth.php';
-
-
-
-
-// From AJCController
-Route::get('/internet', [AJCController::class, 'internet'])->name('internet');
-Route::get('/promos', [AJCController::class, 'promos'])->name('promos');
-Route::get('/contactus', [AJCController::class, 'contactus'])->name('contactus');
-Route::get('/applynow', [AJCController::class, 'applynow'])->name('applynow');
-Route::post('/applynow', [ApplicationController::class, 'applynowPOST'])->name('applynowPOST');
-
 
 
 // Mail Contact US
