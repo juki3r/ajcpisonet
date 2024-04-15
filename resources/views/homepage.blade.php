7da3d3d8b149658f0d2ddc0f350e4919f498242a
juki3r <deepong25@gmail.com>
2024-04-15 09:26:48 +0800

    modified:   resources/views/homepage.blade.php

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -179,7 +179,7 @@
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
             <path d="M6 12.796V3.204L11.481 8zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753"/>
           </svg>
-          Includes 1 month advanced subscription
+          With 1 month advanced subscription
         </p>
       </div>
       <div class="col-12 col-lg-3 col-md-6 py-3  px-5 px-lg-2 px-xl-0 px-md-0 d-flex align-items-center justify-content-center">
