03197a37ebf9b993098e5758b569d352969e144d
juki3r <deepong25@gmail.com>
2024-04-15 10:43:40 +0800

    modified:   resources/views/applynow.blade.php

--- resources/views/applynow.blade.php
+++ resources/views/applynow.blade.php
@@ -13,7 +13,7 @@
                     <h3 class="text-center mb-5">APPLICATION FORM</h3>
                     <div class="mb-2">
                       <label for="name" class="form-label">Full name</label>
-                      <input type="text" class="form-control text-warning"  style="background-color:rgba(255, 255, 255, 0.1)">
+                      <input type="text" class="form-control text-warning"  style="background-color:rgba(255, 255, 255, 0.0)">
                     </div>
                     <div class="mb-2">
                         <label for="phone" class="form-label">Phone number</label>
