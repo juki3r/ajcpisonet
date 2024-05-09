a8200ac8a451a601e413217692b00d97412a6aa7
juki3r <deepong25@gmail.com>
2024-05-09 17:53:25 +0800

    modified:   resources/views/include/homeautomation.blade.php 	modified:   routes/web.php

--- resources/views/include/homeautomation.blade.php
+++ resources/views/include/homeautomation.blade.php
@@ -1,1 +1,50 @@
-<h1>Home Automation</h1>
+
+
+<?php
+    $host = "localhost";		         // host = localhost because database hosted on the same server where PHP files are hosted
+    $dbname = "u539703395_home";              // Database name
+    $username = "u539703395_home";		// Database username
+    $password = "Durt3211631!";	        // Database password
+
+
+// Establish connection to MySQL database
+$conn = new mysqli($host, $username, $password, $dbname);
+
+
+// Check if connection established successfully
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+else { echo "Connected to mysql database. "; }
+
+   
+// // Get date and time variables
+//     date_default_timezone_set('Asia/Kolkata');  // for other timezones, refer:- https://www.php.net/manual/en/timezones.asia.php
+//     $d = date("Y-m-d");
+//     $t = date("H:i:s");
+    
+// // If values send by NodeMCU are not empty then insert into MySQL database table
+
+//   if(!empty($_POST['switch1']))
+//     {
+// 		$val = $_POST['switch1'];
+// // Update your tablename here
+// 	        $sql = "INSERT INTO demo (switch1, Date, Time) VALUES ('".$val."', '".$d."', '".$t."')"; 
+ 
+
+
+// 		if ($conn->query($sql) === TRUE) {
+// 		    echo "Values inserted in MySQL database table.";
+// 		} else {
+// 		    echo "Error: " . $sql . "<br>" . $conn->error;
+// 		}
+// 	}
+
+
+// Close MySQL connection
+$conn->close();
+
+
+
+?>
