e2e4b41d8949f715d1472e1cd024c407a3558bd9
juki3r <deepong25@gmail.com>
2024-05-13 10:17:46 +0800

    modified:   resources/views/homepage.blade.php

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -161,7 +161,7 @@
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
             <path d="M6 12.796V3.204L11.481 8zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753"/>
           </svg>
-          Application form via online <a href="#" class="text-light">Click here</a>
+          Application form via online <a href="applynow" class="text-light">Click here</a>
         </p>
         <p class="ms-3 ms-lg-5 m-0">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
