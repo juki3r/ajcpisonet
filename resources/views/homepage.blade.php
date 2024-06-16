922a053f8c475c44f81fd5357ae78d8f4dbe4f7d
juki3r <deepong25@gmail.com>
2024-06-16 19:01:58 +0800

    modified:   resources/views/homepage.blade.php

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -214,8 +214,8 @@
           <div class="card-body d-flex bg-white flex-column justify-content-space-between">
             <h1 class=" position-relative fw-bold"> <span style="font-size: 20px;position:absolute;margin-left:-15px;margin-top:10px;" class="pesosign">&#8369;</span>1,000</h1>
             <p class="" style="margin-top:-14px">Monthly</p>
-            <p>Unlimited Home Internet</p>
-            <p>Up to 10 Gadgets can connect</p>
+            <p class="p-0 m-0">Unlimited Home Internet</p>
+            <p class="p-0 m-0">Up to 10 Gadgets can connect</p>
           </div>
           <div class="card-footer border-0 bg-white">
             <a href="/applynow800" type="button" class="btn btn-warning  m-auto py-2 px-3 mb-4">Apply Now</a>
@@ -231,7 +231,7 @@
           <div class="card-body d-flex bg-white flex-column justify-content-space-between">
             <h1 class=" position-relative fw-bold"> <span style="font-size: 20px;position:absolute;margin-left:-15px;margin-top:10px;" class="pesosign">&#8369;</span>1,300</h1>
             <p class="" style="margin-top:-14px">Monthly</p>
-            <p>Unlimited Home Internet</p>
+            <p >Unlimited Home Internet</p>
             <p>Up to 15 Gadgets can connect</p>
           </div>
           <div class="card-footer border-0 bg-white">
