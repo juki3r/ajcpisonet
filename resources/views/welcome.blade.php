8c7398ae394f10f2c4e6231ebac129e8e2e4fdec
juki3r <deepong25@gmail.com>
2025-11-07 09:28:26 +0800

    aa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -340,7 +340,7 @@
           </div>
         </div>
         <div class="col-12 col-md-8 p-0 m-0">
-          <div class="row">
+          <div class="row h-50">
             <div class="col-12 col-md-6 bg-success p-2">
               <h2>
                 <img src="{{asset('images/isp.png')}}" alt="" width="100" height="100">
@@ -352,7 +352,7 @@
                 Back-up Power</h2>
             </div>
           </div>
-          <div class="row">
+          <div class="row h-50">
             <div class="col-12 col-md-6 bg-primary p-2">
               <h2>
                 <img src="{{asset('images/affordable.png')}}" alt="" width="100" height="100">
