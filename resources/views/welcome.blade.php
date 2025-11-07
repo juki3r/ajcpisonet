3e68a29543274569238669c72fd9c8b21beee4d5
juki3r <deepong25@gmail.com>
2025-11-07 08:45:04 +0800

    aa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -340,11 +340,13 @@
           </div>
         </div>
         <div class="col-12 col-md-8 p-0 m-0">
-          <div class="col-12 col-md-6 bg-success">
-            <h2>Redundant Internet</h2>
-          </div>
-          <div class="col-12 col-md-6 bg-warning">
-            <h2>Back-up Power</h2>
+          <div class="row">
+            <div class="col-12 col-md-6 bg-success">
+              <h2>Redundant Internet</h2>
+            </div>
+            <div class="col-12 col-md-6 bg-warning">
+              <h2>Back-up Power</h2>
+            </div>
           </div>
         </div>
       </div>
